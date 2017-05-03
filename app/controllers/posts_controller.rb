0f77d2a395a48f55b4dc3d2a3ab7bca55aa9c958
Agustin Silva <silva.agustinn@gmail.com>
2017-05-03 16:18:46 -0300

    Agrega CRUD de Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_usuario.posts.build(post_params)
 
     if @post.save
       redirect_to @post, notice: 'Post was successfully created.'
